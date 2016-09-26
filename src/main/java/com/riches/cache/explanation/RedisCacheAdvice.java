b2a113ad0fc0e2a942a4ce7a16d33540ccc22142
37809131@qq.com <37809131@qq.com>
2016-09-26 22:02:15 +0800

    代码review

--- src/main/java/com/riches/cache/explanation/RedisCacheAdvice.java
+++ src/main/java/com/riches/cache/explanation/RedisCacheAdvice.java
@@ -17,7 +17,7 @@ import com.riches.cache.client.RedisClient;
 @Component
 @Aspect
 public class RedisCacheAdvice {
-	
+
 	@Resource
 	private RedisClient redisClient;
 
@@ -27,28 +27,27 @@ public class RedisCacheAdvice {
 	}
 
 	@Around("getPonitcut()")
-	public Object cacheGetSingle(final ProceedingJoinPoint pjp)
-			throws Throwable {
+	public Object cacheGetSingle(final ProceedingJoinPoint pjp) throws Throwable {
+
+		Signature sig = pjp.getSignature();
+
+		MethodSignature msig = null;
+		if (!(sig instanceof MethodSignature)) {
+			throw new IllegalArgumentException("该注解只能用于方法");
+		}
+		msig = (MethodSignature) sig;
+		Object target = pjp.getTarget();
+		Method currentMethod = target.getClass().getMethod(msig.getName(), msig.getParameterTypes());
+
+		int expiration = currentMethod.getAnnotation(RedisCache.class).expiration();
+
+		// 根据类名，方法名和参数生成key
 		// 得到类名、方法名和参数
 		String clazzName = pjp.getTarget().getClass().getName();
-		String methodName = pjp.getSignature().getName();
+		String methodName = sig.getName();
 		Object[] args = pjp.getArgs();
-		
-		
-		Signature sig = pjp.getSignature();
-        MethodSignature msig = null;
-        if (!(sig instanceof MethodSignature)) {
-            throw new IllegalArgumentException("该注解只能用于方法");
-        }
-        msig = (MethodSignature) sig;
-        Object target = pjp.getTarget();
-        Method currentMethod = target.getClass().getMethod(msig.getName(), msig.getParameterTypes());
-        
-        int expiration = currentMethod.getAnnotation(RedisCache.class).expiration();
-        
-		// 根据类名，方法名和参数生成key
 		String key = genKey(clazzName, methodName, args);
-		System.out.println(key);
+
 		Object obj = null;
 		if (redisClient.exists(key)) {
 			obj = redisClient.get(key, Object.class);
